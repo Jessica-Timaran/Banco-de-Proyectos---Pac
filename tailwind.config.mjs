b335b2fa33755e3b32158ed1f7f277832c39c6a4
Kadayana <160735145+Kadayana@users.noreply.github.com>
2024-06-07 18:48:53 -0500

    Avance con lenguaje Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,17 @@
 export default {
 	content: [
-	  './src/**/*.{astro,html,js,jsx,md,mdx,svelve,ts,tsx,vue}', 
+	'./src/**/*.{astro,html,js,jsx,md,mdx,svelve,ts,tsx,vue}',
 	],
 	theme: {
-	  extend: {
+	extend: {
 		colors: {
-		  Verde: '#A3E784'
+		Verde: '#A3E784'
 		},
 		fontFamily: {
-		  'Inter': ['Inter', 'sans-serif'],
+		'Inter': ['Inter', 'sans-serif'],
 		},
-	  },
+	},
 	},
 	variants: {},
 	plugins: [],
-  }
-  
+}
