63585374f96302bfd3534384d54b4569f70fbd5e
Karen <Kadayana51@gmail.com>
2024-06-07 11:47:29 -0500

    Avance con lenguaje Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+export default {
+	content: [
+	  './src/**/*.{astro,html,js,jsx,md,mdx,svelve,ts,tsx,vue}', 
+	],
+	theme: {
+	  extend: {
+		colors: {
+		  Verde: '#A3E784'
+		},
+		fontFamily: {
+		  'Inter': ['Inter', 'sans-serif'],
+		},
+	  },
+	},
+	variants: {},
+	plugins: [],
+  }
+  
