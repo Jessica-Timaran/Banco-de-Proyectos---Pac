278ca8a718b5af602d1e4dece0b3b162b2bd6d0a
Kadayana <160735145+Kadayana@users.noreply.github.com>
2024-06-12 18:02:11 -0500

    Avances SuperAdmin
    Se realizaron modificaciones en la pagina de home de SuperAdmin y home inicial, se creo el Navbar Temporal y agrego Imagen, Se agregaron colores y se modifico el input, se comenzó vista de Crear Usuario

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,11 +7,17 @@ export default {
 		colors: {
 		'Verde': '#A3E784',
 		'Color_carta': '#FBFCFF',
-		'Verde_claro': '#A3E784'
+		'Verde_claro': '#A3E784',
+		'Gris_Oscuro': '#F5F6FA',
+		'Borde_gris':'#B9B9B9',
+
 		},
 		fontFamily: {
 		'Inter': ['Inter', 'sans-serif'],
 		},
+		maxWidth: {
+        'max-1200': '1200px',
+      },
 	},
 	},
 	variants: {},
