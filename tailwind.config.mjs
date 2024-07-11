80cd94775401058aaaab8831618d7ca83ddba27f
Karen <Kadayana51@gmail.com>
2024-07-11 08:15:31 -0500

    Todas las paginas y conexiones

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,8 @@
 
 export default {
 	content: [
-	'./src/**/*.{astro,html,js,jsx,md,mdx,svelve,ts,tsx,vue}',
-	],
+	'./src/**/*.{astro,html,js,jsx,md,mdx,svelve,ts,tsx,vue}'],
+	darkMode: 'class', // o 'media'
 	theme: {
 	extend: {
 		colors: {
@@ -11,6 +11,7 @@ export default {
 		'Verde_claro': '#A3E784',
 		'Gris_Oscuro': '#F5F6FA',
 		'Borde_gris':'#B9B9B9',
+		'custom-green': 'rgba(163, 231, 132, 1)',
 
 		},
 		fontFamily: {
