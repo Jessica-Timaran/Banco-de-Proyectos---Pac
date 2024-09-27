353fbab9e106da71e1eeca435af8571091061484
Kadayana <160734989+Jessica-Timaran@users.noreply.github.com>
2024-09-27 15:47:36 -0500

    SuperAdmin

--- Frontend/src/Pages/SuperAdmin/AdminProyectos.jsx
+++ Frontend/src/Pages/SuperAdmin/AdminProyectos.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import LayoutPrincipal from '../../layouts/LayoutPrincipal';
+import LayoutPrincipal from '../../layouts/LayoutPrincipal1';
 import Layoutcontenido from '../../Layouts/Layoutcontenido4';
 import GridListProyectos from './GridList/GridListProyectos';
 import Loader from '../../Components/Loader';
