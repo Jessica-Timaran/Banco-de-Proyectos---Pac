def3ad69043acd72b80a50b64d57954915fadf40
Jessica-Timaran <jessicatimaran1@gmail.com>
2024-09-30 18:21:08 -0500

    cambio de icono

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -7,15 +7,15 @@ const Navbar = ({ Text }) => {
   const { user } = useUser(); // Usa el contexto del usuario
 
   return (
-    <nav className="bg-Color_carta flex p-4 justify-between z-40">
-    <div className="flex items-start z-20">
+    <nav className="bg-Color_carta flex p-4 justify-between z-40 h-20  ">
+    <div className="flex items-start z-20 ">
       {/* <Buscador type="search" /> */}
     </div>
-    <div className="flex grid-cols-2 justify-center items-center mr-6">
+    <div className="flex grid-cols-2 justify-center items-center mr-6 ">
         <img
            src={logoUser}
           alt="User Icon"
-          className="flex rounded-full w-12 h-12 mr-2 justify-end"
+          className="flex rounded-full w-7 h-7 mr-2 justify-end"
         />
         <span className="text-black">
           {user.nombre || 'Invitado'} {/* Mostrar el nombre del usuario o "Invitado" */}
