707bf745c667288fdf611bbac96224622af88fe7
Jessica-Timaran <jessicatimaran1@gmail.com>
2024-10-01 21:49:11 -0500

    manejo de errores

--- Backend/app.js
+++ Backend/app.js
@@ -37,14 +37,16 @@ app.use('/api/user', userRoutes);
 app.use('/api/promedioFinal', promedioFinal);
 app.use('/api/promedio', obtenerPromedio);
 
-// Manejo de errores 1
-// app.use((err, req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'https://bancodeproyectospac.netlify.app'); // Ajusta al dominio exacto
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     next();
-// });
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://bancodeproyectospac.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 // Manejo de errores
 app.use((err, req, res, next) => {
