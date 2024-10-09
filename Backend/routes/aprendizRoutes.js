f205b1e7938da0d7466c0ee2d5fd903e4eebac56
Yurany De La Cruz <yuranidelacruz2006@gmail.com>
2024-10-09 07:01:59 -0500

    olvidar contraseña y update

--- Backend/routes/aprendizRoutes.js
+++ Backend/routes/aprendizRoutes.js
@@ -53,7 +53,7 @@ router.post('/reset-password', async (req, res) => {
 
       // Genera el token
       const resetToken = generateToken(email); 
-      const resetLink = `https://6705ef0148f7da0008cc5e73--bancodeproyectospac.netlify.app/Principal/UpdatePassword?token=${resetToken}&email=${encodeURIComponent(email)}`;
+      const resetLink = `https://67066fbcc13f7e0008e8dd07--bancodeproyectospac.netlify.app/Principal/UpdatePassword?token=${resetToken}&email=${encodeURIComponent(email)}`;
 
       const mailOptions = {
           from: 'pac.bancodeproyectos@gmail.com',
